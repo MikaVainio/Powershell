57ca2594df4cb7d429dc67285ef224e19a59979b
MikaVainio <koti.mika.vainio@gmail.com>
2017-01-14 14:07:47 +0200

    Add files via upload

---
+++ KomentosovelmaKaikkiRymätKäyttäjineen.ps1
@@ -0,0 +1,71 @@
+﻿Function Get-ADUserGroupDocument # Julkinen funktio --> komentosovelma
+{ # funktiomääritysten alku
+[CmdletBinding()] # Laajennettuparametrimäärittely 
+param
+( # parametriluettelon alku
+    # Pakollinen parametri, ei putkituksen tukea, koska vain yksi tiedosto, johon tallennetaan
+    [Parameter(Mandatory=1, ValueFromPipeline=0, ValueFromPipelineByPropertyName=0)]
+    [String]$Tiedosto # parametri on määritelty merkkijonoksi  
+) # parametriluettelon loppu
+    BEGIN # Aloituslohko, jossa työfunktio määritellään
+    { # Begin-lohkon alku
+        function AdkäyttäjäRyhmä # Työfunktion määrittely, yksityinen (private)
+        { # työfunktiomääritysten alku
+        # Haetaan kaikki AD:n ryhmät
+        $KaikkiRyhmät = Get-ADGroup -Filter *
+        # Määritellään vektori tulosobjektin tallentamiseen ja alustetaan se tyhjäksi
+        $Tulosvektori = @()
+        # Käydään ryhmät yksitellen läpi: ulompi silmukka
+        foreach($Ryhmä in $KaikkiRyhmät)
+            { # ulomman silmukan alku
+            # Haetaan  kaikki ryhmän jäsenobjektit muuttujaan $Jäsenet
+            $Jäsenet = Get-ADGroupMember -Identity $Ryhmä.SamAccountName
+            # Käydään kaikki jäsenet yksitellen läpi ja muodostetaan uudet objetit: sisempi silmukka
+            forEach($Jäsen in $Jäsenet)
+                { # sisemmän silmukan alku
+                # Luodaan uusi PSObjekti $Tulosobjekti
+                $TulosObjekti = New-Object -TypeName PSObject
+                # Lisätään objektiin ominaisuutena Ryhmä ja annetaan sille arvoksi Ryhmäobjektin SamAccountName
+                $TulosObjekti | Add-Member -MemberType NoteProperty -Name Ryhmä -Value $Ryhmä.SamAccountName
+                # Lisätään objektiin ominaisuuksiksi jäsenten tietoja objektimuuttujasta $Jäsen
+                $TulosObjekti | Add-Member -MemberType NoteProperty -Name Tunnus -Value $Jäsen.Name
+                $TulosObjekti | Add-Member -MemberType NoteProperty -Name Tyyppi -Value $Jäsen.ObjectClass
+                # Jos jäsen on käyttäjä, lisätään sähköpostiosoite objektiin
+                if($Jäsen.ObjectClass -eq "user") # huom. -eq, ei =-merkki
+                    {
+                        # Haetaan Get-ADUser-komennolla jäsenen s-postiosoite SamAccountName-ominaisuuden avulla 
+                        $Käyttäjä = Get-ADUser -Identity $Jäsen.SamAccountName -Properties EmailAddress
+                        $Sähköposti = $Käyttäjä.EmailAddress
+                    }
+                else
+                    {
+                        $Sähköposti = "N/A" # Muille objektityypeille arvoksi N/A
+                    }
+                $TulosObjekti | Add-Member -MemberType NoteProperty -Name Sposti -Value $Sähköposti
+                $Tulosvektori += $TulosObjekti # Lisätään tulosobjekti tulosvektoriin
+                } # sisemmän silmukan loppu
+            } # ulomman silmukan loppu
+        Write-Output $Tulosvektori # palautetaan työfunktion muodostama objekti
+        } # työfunktiomääritysten loppu
+    } # Begin-lohkon loppu
+    
+
+    PROCESS # Työlohko, jossa työfunktiota kutsutaan
+    { # process-lohkon alku
+    # Varoitetaan käyttäjää siitä, että toiminto saattaa kestää kauan
+    Write-Warning "Käsitellään kaikki ryhmät ja käyttäjät AD:sta, tämä voi kestää jonkin aikaa..."
+    # Kutsutaan työfunktiota ja putkitetaan tulos julkisen funktion parametrina annettuun tiedostoon
+    AdkäyttäjäRyhmä | Export-Csv $Tiedosto -Encoding Unicode -Delimiter ";"
+    } # process-lohkon loppu
+
+
+
+    END # Lopetuslohko
+    { # end-lohkon alku
+    Write-Host "Ryhmäjäsenyydet käsitelty ja tallennettu tiedostoon $Tiedosto"
+    } # end-lohkon loppu
+} # funktiomääritysten loppu
+
+# Testaus, joka poistetaan tallennettaessa funktiota komentosovelmaksi
+Get-ADUserGroupDocument -Tiedosto "C:\Users\Administrator\Documents\ADRDoc.txt"
+
---
+++ KäyttöOikeusesimerkki.ps1
@@ -0,0 +1,54 @@
+﻿# SKRIPTI KOTIHAKEMISTOJEN LUOMISEEN PALVELIMEN VERKKOJAKOON
+# Skriptissä käytetään komentosovelmia ja DLL-kirjastoja, jotka on ladattava erikseen
+# Ladattavat komentosovelmat löytyvät osoitteesta https://gallery.technet.microsoft.com/scriptcenter/1abd77a5-9c0b-4a2b-acef-90dbb2b84e85
+# Latauksen jälkeen komentosovelmat on purettava ja tallennettava ~\Documents\WindowsPowerShell\Modules\NTFSSecurity -kansioon
+# Määritellään muuttuja kotihakemistojen päähakemistolle
+$Jako = Read-Host "Anna kotihakemistojen päähakemiston nimi ja polku"
+# Luodaan päähakemisto
+New-Item -Path $Jako -ItemType directory
+# Määritellään muuttuja päähakemiston jakonimelle
+$Jakonimi = Read-Host "Anna päähakemiston jakonimi"
+# Jaetaan päähekemisto
+New-SmbShare -Name $Jakonimi -Path $Jako -FullAccess Everyone
+# Haetaan kaikki käyttäjätilit muuttujaan 
+$Käyttäjät = Get-ADUser -Filter *
+# Käsitellään kaikki käyttäjät silmukassa
+foreach($Käyttäjä in $Käyttäjät)
+{
+    # Luodaan muuttuja $Kansio, joka on sekä käyttäjän että hakmiston nimi
+    $Kansio = $Käyttäjä.SamAccountName
+    # Luodaan kansio kotihakemistoa varten
+    New-Item -Path "$Jako\$Kansio" -ItemType directory
+    # Katkaistaan käyttöoikeuksien periytyminen, jotta muiden käyttäjien oikeuksia voidaan poistaa
+    Disable-NTFSAccessInheritance -Path "$Jako\$Kansio"
+    #Annetaan käyttäjälle täydet oikeudet kansioon
+    Add-NTFSAccess -Path "$Jako\$Kansio" -AccessRights FullControl -AccessType Allow -Account $Kansio -AppliesTo ThisFolderSubfoldersAndFiles
+    #Poistetaan Users-käyttäjäryhmän periytyneet oletusoikeudet kansioon
+    Remove-NTFSAccess -Path "$Jako\$Kansio" -AccessRights Read,ReadAndExecute,ListDirectory,CreateFiles,AppendData,Synchronize -Account Users
+    }
+
+# Palautetaan syntyneiden kansioiden käyttöoikeustiedot
+# Luodaan tyhjä vektorimuuttuja käyttöoikeuksien näyttämistä varten
+$Tulosvektori = @()
+# Listataan alihakemistorakenne muuttujaan
+$Alihakemistot = Get-ChildItem $Jako -Recurse
+foreach($Alihakemisto in $Alihakemistot)
+{
+    $Polku = $Alihakemisto.FullName
+    $Oikeudet = Get-NTFSAccess -Path $Polku
+    foreach($Oikeus in $Oikeudet)
+    {
+    # Luodaan uusi PSObjekti $Tulosobjekti
+    $TulosObjekti = New-Object -TypeName PSObject
+    # Lisätään objektiin ominaisuutena hakemiston nimi
+    $TulosObjekti | Add-Member -MemberType NoteProperty -Name Hakemisto -Value $Polku
+    # Lisätään objektiin ominaisuuksiksi jäsenten tietoja objektimuuttujasta $Jäsen
+    $TulosObjekti | Add-Member -MemberType NoteProperty -Name Tili -Value $Oikeus.Account
+    $TulosObjekti | Add-Member -MemberType NoteProperty -Name Oikeus -Value $Oikeus.AccessRights
+    $TulosObjekti | Add-Member -MemberType NoteProperty -Name Peritty -Value $Oikeus.IsInherited
+    $TulosObjekti | Add-Member -MemberType NoteProperty -Name Hakemistosta -Value $Oikeus.InheritedFrom
+    #Lisätään Tulosobjekti Tulosvektoriin
+    $Tulosvektori += $TulosObjekti
+    }
+}
+Write-Output $Tulosvektori | Out-GridView
