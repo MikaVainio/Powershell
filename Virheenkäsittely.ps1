a2b15aa0a5f9f524000c0235c172adc2bac983f7
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-02 13:43:03 +0300

    Add files via upload

---
+++ Virheenkäsittely.ps1
@@ -0,0 +1,26 @@
+﻿# Esimerkki virheenkäsittelyn perusrutiinista
+$Käyttäjä = "olematon"
+# Try-lohkoon sijoitetaan toiminto, joka halutaa suorittaa
+Try
+{
+    # Haetaan käyttäjän tiedot, virhetilanteessa lopetaan
+    Get-ADUser -Identity $Käyttäjä -ErrorAction Stop
+    $Tila = "onnistui"
+}
+# Catch-lohkoon sijoitetaan virheen tapahduttua suoritettavat toiminnot
+Catch
+{
+    # Tallennetaan komennon virheviesti muuttujaan
+    $Virheilmoitus = $_.Exception.Message
+    # Annetaan virheilmoitus ja muutetaan teksti punaiseksi
+    Write-Error -Message $Virheilmoitus
+    $Tila = "epäonnistui"
+    Break
+}
+# Finally lohko suoritetaan molemmissa tapauksissa
+Finally
+{
+    # Kirjoitetaan viesti oranssilla tekstillä
+    Write-Warning "Operaatio $Tila"
+}
+    
