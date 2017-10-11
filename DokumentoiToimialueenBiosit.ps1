d5b5371411345002deef6de41ed777ac888d7254
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-11 16:27:59 +0300

    Add files via upload

---
+++ DokumentoiToimialueenBiosit.ps1
@@ -0,0 +1,27 @@
+﻿# Dokumentoidaan kaikkien koneiden BIOS-versiot
+# Tyhä vektori tuloksille
+$Output = @()
+# Luetaan konetilit muuttujaan 
+$Koneet = Get-ADComputer -Filter *
+# Käydään konetilit yksitellen läpi
+foreach($Kone in $Koneet)
+{
+    $KoneNimi = $Kone.Name
+    $KoneDNSNimi = $Kone.DNSHostName
+    $KoneTyyppi = $Kone.ObjectClass
+
+    $KoneBiosTieto = Get-WmiObject -Class Win32_BIOS -ComputerName $KoneNimi
+    
+        # Määritellään uuden objektin ominaisuudet
+        $KoneBIOSValmistaja = $KoneBiosTieto.Manufacturer
+        $KoneBIOSVersio = $KoneBiosTieto.SMBIOSBIOSVersion
+        $Ominaisuudet = @{"Name" = $KoneNimi ; "DNSHostName" = $KoneDNSNimi`        ;"ObjectClass" = $KoneTyyppi ; "Manufacturer" = $KoneBIOSValmistaja ;`        "SMBIOSVersion" = $KoneBIOSVersio}
+        # Luodaan uusi objekti
+        $Objekti = New-Object -TypeName PSObject -Property $Ominaisuudet
+        # Lisätään objekti vektoriin
+        $Output = $Output + $Objekti
+    
+}
+$Output | Write-Output
+
+
