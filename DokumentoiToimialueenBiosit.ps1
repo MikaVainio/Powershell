1c9b2bccae411db52078059b213a7786bb8955bd
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-12 11:48:30 +0300

    Update DokumentoiToimialueenBiosit.ps1
    Lisätty kommentteja ja siivottu ylimääräisiä tyhjiä rivejä

--- DokumentoiToimialueenBiosit.ps1
+++ DokumentoiToimialueenBiosit.ps1
@@ -12,36 +12,34 @@ foreach($Kone in $Koneet)
     $KoneNimi = $Kone.Name
     $KoneDNSNimi = $Kone.DNSHostName
     $KoneTyyppi = $Kone.ObjectClass
-
     # Yritetään muodostaa yhteys koneeseen ja selvittää BIOS-tiedot
     try
     {
         $KoneBiosTieto = Get-WmiObject -Class Win32_BIOS -ComputerName $KoneDNSNimi -ErrorAction Stop
-    
-        # Määritellään uuden objektin ominaisuudet
+        # Määritellään muuttujien arvot, kun tiedot saadaan
         $KoneBIOSNimi = $KoneBiosTieto.Name
         $KoneBIOSValmistaja = $KoneBiosTieto.Manufacturer
         $KoneBIOSVersio = $KoneBiosTieto.SMBIOSBIOSVersion
-        $KoneStatus = "Connected"
-        
+        $KoneStatus = "Connected" 
     }
     catch
     {
+        # Määritellään muuttujien arvot, kun tietoja ei saada
         Write-Warning "Koneeseen $KoneDNSNimi ei saatu yhteyttä"
         $KoneStatus = "Not connected"
         $KoneBIOSNimi = "NA"
         $KoneBIOSValmistaja = "NA"
         $KoneBIOSVersio = 0
-
     }
     finally
     {
+        # Määritellään uuden objektin ominaisuudet
         $Ominaisuudet = @{"Name" = $KoneNimi ; "DNSHostName" = $KoneDNSNimi`
         ;"ObjectClass" = $KoneTyyppi ; "Manufacturer" = $KoneBIOSValmistaja ;`
         "BIOSName" = $KoneBIOSNimi ; "SMBIOSVersion" = $KoneBIOSVersio; "Status" = $KoneStatus}
         # Luodaan uusi objekti
         $Objekti = New-Object -TypeName PSObject -Property $Ominaisuudet
-        # Lisätään objekti vektoriin
+        # Lisätään objekti tulosvektoriin
         $Output = $Output + $Objekti
     }
 }
