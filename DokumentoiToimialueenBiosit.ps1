da0abb9aee0a3f466f419ab66a32e67cea6a7d8f
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-12 11:39:50 +0300

    Update DokumentoiToimialueenBiosit.ps1
    Lisätty kommennteja ja BIOS-nimitieto, muutettu tulosobjektin muodostustapaa

--- DokumentoiToimialueenBiosit.ps1
+++ DokumentoiToimialueenBiosit.ps1
@@ -1,4 +1,7 @@
-﻿# Dokumentoidaan kaikkien koneiden BIOS-versiot
+# Dokumentoidaan kaikkien koneiden BIOS-versiot
+# Kohdekoneiden palomuurin pitää sallia WMI-yhteydet
+# Ota käyttöön sääntö: Windows Management Instrumentation (WMI-In)
+# 
 # Tyhä vektori tuloksille
 $Output = @()
 # Luetaan konetilit muuttujaan 
@@ -10,18 +13,37 @@ foreach($Kone in $Koneet)
     $KoneDNSNimi = $Kone.DNSHostName
     $KoneTyyppi = $Kone.ObjectClass
 
-    $KoneBiosTieto = Get-WmiObject -Class Win32_BIOS -ComputerName $KoneNimi
+    # Yritetään muodostaa yhteys koneeseen ja selvittää BIOS-tiedot
+    try
+    {
+        $KoneBiosTieto = Get-WmiObject -Class Win32_BIOS -ComputerName $KoneDNSNimi -ErrorAction Stop
     
         # Määritellään uuden objektin ominaisuudet
+        $KoneBIOSNimi = $KoneBiosTieto.Name
         $KoneBIOSValmistaja = $KoneBiosTieto.Manufacturer
         $KoneBIOSVersio = $KoneBiosTieto.SMBIOSBIOSVersion
-        $Ominaisuudet = @{"Name" = $KoneNimi ; "DNSHostName" = $KoneDNSNimi`        ;"ObjectClass" = $KoneTyyppi ; "Manufacturer" = $KoneBIOSValmistaja ;`        "SMBIOSVersion" = $KoneBIOSVersio}
+        $KoneStatus = "Connected"
+        
+    }
+    catch
+    {
+        Write-Warning "Koneeseen $KoneDNSNimi ei saatu yhteyttä"
+        $KoneStatus = "Not connected"
+        $KoneBIOSNimi = "NA"
+        $KoneBIOSValmistaja = "NA"
+        $KoneBIOSVersio = 0
+
+    }
+    finally
+    {
+        $Ominaisuudet = @{"Name" = $KoneNimi ; "DNSHostName" = $KoneDNSNimi`
+        ;"ObjectClass" = $KoneTyyppi ; "Manufacturer" = $KoneBIOSValmistaja ;`
+        "BIOSName" = $KoneBIOSNimi ; "SMBIOSVersion" = $KoneBIOSVersio; "Status" = $KoneStatus}
         # Luodaan uusi objekti
         $Objekti = New-Object -TypeName PSObject -Property $Ominaisuudet
         # Lisätään objekti vektoriin
         $Output = $Output + $Objekti
-    
+    }
 }
-$Output | Write-Output
-
+$Output | Out-GridView
 
