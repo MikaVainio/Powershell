537633839238b51c3e3bf42630cb8070097e474f
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-13 19:54:35 +0300

    Update AjastettuTaustaAjo.ps1

--- AjastettuTaustaAjo.ps1
+++ AjastettuTaustaAjo.ps1
@@ -1,8 +1,8 @@
 ﻿# Suoritetaan ajastetusti keskiviikkoisin
-# klo 19.15 käyttäjien dokumentointi tausta-ajona
+# klo 19.30 käyttäjien dokumentointi tausta-ajona
 $Skriptipolku = "C:\Users\Administrator\Documents\Jobi.ps1"
 $PSPolku = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-$Työ = New-ScheduledTaskAction -Execute $PSPolku -Argument "-NonInteractive -NoLogo -NoProfile -File $Skriptipolku"
+$Toiminto = New-ScheduledTaskAction -Execute $PSPolku -Argument "-NonInteractive -NoLogo -NoProfile -File $Skriptipolku"
 $Käynnistys = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Wednesday -At 19:30
-$Homma = New-ScheduledTask -Action $Työ -Trigger $Käynnistys -Settings (New-ScheduledTaskSettingsSet)
-$Homma | Register-ScheduledTask -TaskName "Ajastettu käyttäjien dokumentointi" -User "Administrator" -Password "Q2werty"
+$Tehtävä = New-ScheduledTask -Action $Toiminto -Trigger $Käynnistys -Settings (New-ScheduledTaskSettingsSet)
+$Tehtävä | Register-ScheduledTask -TaskName "Ajastettu käyttäjien dokumentointi" -User "Administrator" -Password "Q2werty"
