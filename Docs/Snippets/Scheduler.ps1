c68146ddab4107991aed54258f006fd0e4484a09
Mika Vainio <koti.mika.vainio@gmail.com>
2017-10-14 19:34:50 +0300

    Update Scheduler.ps1
    -Weekly parametri tarvitaan viikonpäiviin perustuvassa käynnistyksessä.

--- Docs/Snippets/Scheduler.ps1
+++ Docs/Snippets/Scheduler.ps1
@@ -5,7 +5,7 @@ $PSPolku = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 # Määritellään suoritettava toiminto
 $Toiminto = New-ScheduledTaskAction -Execute $PSPolku -Argument "-NonInteractive -NoLogo -NoProfile -File $Skripti"
 # Määritellään ajoaikataulu
-$Käynnistys = New-ScheduledTaskTrigger -DaysOfWeek Saturday -At "22:00"
+$Käynnistys = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Saturday -At "22:00"
 # Määritellään uusi työ scheduler-ohjelman työjonoon
 $Työ = New-ScheduledTask -Action $Toiminto -Trigger $Käynnistys -Settings (New-ScheduledTaskSettingsSet)
 # Rekisteröidään työ scheduler-ohjelmaan
