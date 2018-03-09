61a2e8d6a732aeaa3444b92cca5be389dfce7ca5
Mika Vainio <koti.mika.vainio@gmail.com>
2018-03-09 13:52:19 +0200

    Lisätty kommentti
    Muistutus siitä, että vain asennettavat ohjelmat löytyvät listasta.

--- DokumentoiAsennetutOhjelmat.ps1
+++ DokumentoiAsennetutOhjelmat.ps1
@@ -1,5 +1,6 @@
 ﻿# Dokumentoi asennetut ohjelmat csv-tiedostoon
 $Tiedosto = Read-Host "Anna tiedoston polku ja nimi"
+# Tämä dokumentoi vain ne ohjelmat, joista tulee rekisteriin merkintä
 $Täysilista = Get-WmiObject -Class Win32_Product
 $Tuloslista = $Täysilista | Select-Object -Property Vendor, Name, Version, Language
 $Tuloslista | Export-Csv -Delimiter ";" -Encoding Unicode $Tiedosto
