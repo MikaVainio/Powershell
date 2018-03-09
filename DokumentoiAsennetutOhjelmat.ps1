ed660dacc1875bc110e633c8efed1824fb2c3e9f
Mika Vainio <koti.mika.vainio@gmail.com>
2018-03-09 13:45:08 +0200

    Lisätty uusia kommentteja

--- DokumentoiAsennetutOhjelmat.ps1
+++ DokumentoiAsennetutOhjelmat.ps1
@@ -3,4 +3,5 @@ $Tiedosto = Read-Host "Anna tiedoston polku ja nimi"
 $Täysilista = Get-WmiObject -Class Win32_Product
 $Tuloslista = $Täysilista | Select-Object -Property Vendor, Name, Version, Language
 $Tuloslista | Export-Csv -Delimiter ";" -Encoding Unicode $Tiedosto
+# Ilmoitetaan kun kaikki on valmista
 Write-Warning "Asennetut ohjelmat listattu tiedostoon $Tiedosto"
