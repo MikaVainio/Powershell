e72514d47715d75d1f1e87391bebb51edd6b4074
Mika Vainio <koti.mika.vainio@gmail.com>
2018-03-20 13:37:22 +0200

    Lisätty mallivastaus

---
+++ Docs/Mallivastaus.ps1
@@ -0,0 +1,62 @@
+# Dokumentoidaan kaikkien koneiden asennetut ohjelmat
+# Kohdekoneiden palomuurin pitää sallia WMI-yhteydet
+# Ota käyttöön sääntö: Windows Management Instrumentation (WMI-In)
+
+# Kysytään käyttäjältä tallennustiedoston nimi ja polku
+$Tiedosto = Read-Host "Anna luotavan CSV-tiedoston polku ja nimi"
+
+# Tyhjä vektori tuloksille ja laskurit läpikäytyjen koneiden määrälle
+$Tulosvektori = @()
+$Konemäärä = 0 # Kaikken koneiden määrä
+$Tavoittamattomia = 0 # Koneiden määrä, joihin ei saatu yhteyttä
+
+
+# Luetaan konetilit muuttujaan
+$Koneet = Get-ADComputer -Filter *
+
+# Käydään konetilit yksitellen läpi
+foreach($Kone in $Koneet)
+{
+# Tallennetaan koneen NetBIOS-nimi muuttujaan $KoneNimi
+    $KoneNimi = $Kone.Name
+
+    # Yritetään muodostaa yhteys koneeseen ja selvittää ohjelmien tiedot
+    try
+    {
+    # Luetaan ohjelmien tiedot koneesta
+    $Ohjelmat = Get-WmiObject -Class Win32Win32_Product -ComputerName $KoneNimi -ErrorAction Stop
+    # Käydään koneen ohjelmat yksitellen läpi
+    ForEach($Ohjelma in $Ohjelmat)
+      {
+        # Luodaan uusi PSObjekti $Tulosobjekti
+        $TulosObjekti = New-Object -TypeName PSObject
+        # Lisätään objektiin ominaisuutena KoneNimi
+        $TulosObjekti | Add-Member -MemberType NoteProperty -Name Computer -Value $KoneNimi
+        # Lisätään objektiin ominaisuuksiksi ohelmien tietoja objektimuuttujasta $Jäsen
+        $TulosObjekti | Add-Member -MemberType NoteProperty -Name Vendor -Value ($Ohjelma.Vendor)
+        $TulosObjekti | Add-Member -MemberType NoteProperty -Name Software -Value ($Ohjelma.Name)
+        $TulosObjekti | Add-Member -MemberType NoteProperty -Name Version -Value ($Ohjelma.Version)
+
+        # Lisätään tulosobjekti tulosvektoriin
+        $Tulosvektori = $Tulosvektori + $TulosObjekti
+      }
+
+    }
+    catch
+    {
+        # Ilmoitetaan konsolissa, ettei koneeseen saatu yhteyttä
+        Write-Warning "Laitteeseen $KoneNimi ei saatu yhteyttä"
+        $Tavoittamattomia = $Tavoittamattomia + 1
+
+    }
+    finally
+    {
+        $Konemäärä = $Konemäärä + 1
+    }
+}
+# Tallentaan vektorin tiedot tiedostoon
+$Tulosvektori | Export-Csv -Delimiter ";" -Encoding Unicode $Tiedosto
+
+# Ilmoitetaan tiedot skriptin käyttäjälle
+Write-Warning "Asennetut ohjelmat listattu tiedostoon $Tiedosto"
+Write-Warning "Käsiteltiin $Konemäärä tietokonetta. Yhteyttä ei saatu $Tavoittamattomia koneeseen"
