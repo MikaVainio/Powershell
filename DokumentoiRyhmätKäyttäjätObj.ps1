654b5ad9a2f94773c980bb09adc299533f81b0bc
Mika Vainio <koti.mika.vainio@gmail.com>
2020-03-06 14:54:29 +0200

    Lisätty puhdas objektiesimerkki
    Ryhmien ja niiden jäsenten dokumentointi puhtaana olioratkaisuna

---
+++ DokumentoiRyhmätKäyttäjätObj.ps1
@@ -0,0 +1,41 @@
+﻿# Määritellään luokka ryhmä-jäsen-olioiden luontia varten
+Class UserGroupDoc
+{
+    # Objektin ominaisuudet (kentät)
+    [String] $GroupName
+    [String] $FullName
+    [String] $LoginName
+    [String] $Type
+
+    # Muodostin
+    UserGroupDoc ([String] $GroupName, $FullName, $LoginName, $Type)
+    {
+        $this.GroupName = $GroupName
+        $this.FullName = $FullName
+        $this.LoginName = $LoginName
+        $this.Type = $Type
+
+    }
+}
+
+# Vektori tuloksille, tyhjä
+$GroupsAndMembers = @()
+
+# Ryhmät muuttujaan
+$Groups = Get-ADGroup -filter *
+
+# Käydään ryhmät yksitellen läpi
+Foreach($Group in $Groups)
+{
+    $Members = Get-ADGroupMember -identity $Group.SamAccountName
+
+    # Käydään ryhmän jäsenet yksitellen läpi
+    Foreach($Member in $Members)
+    {
+        # Luodaan uusi objekti ja lisätään se vektoriin
+        $GroupsAndMembers += [UserGroupDoc]::New($Group.SamAccountName, $Member.Name, $Member.SamAccountName, $Member.ObjectClass)
+    }
+}
+
+# Palautetaan objektit vektorista taulukkoon
+$GroupsAndMembers | Out-GridView
