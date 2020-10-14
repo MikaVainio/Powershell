bb8da156f395bc22756d7ce4f4636ab49c511a5d
Mika Vainio <koti.mika.vainio@gmail.com>
2020-10-14 12:43:10 +0300

    Add files via upload

---
+++ LeoMuutaVerkotYksityisiksi.ps1
@@ -0,0 +1,14 @@
+﻿# KONEEN KAIKKIEN VERKKOJEN MÄÄRITYS YKSITYISIKSI
+
+Write-Warning "Kaikki aktiiviset verkot muutetaan yksityisiksi"
+$Vastaus= Read-Host "Haluatko jatkaa K/E"
+if($Vastaus.ToUpper() -eq "K") 
+{
+   Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private 
+} 
+else 
+{
+   Write-Warning "Muutosta ei suoritettu"
+}
+
+Get-NetConnectionProfile | Out-GridView
