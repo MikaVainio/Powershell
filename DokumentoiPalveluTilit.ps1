8562871857c4a826ec35ac8ccb47522a8681782d
Mika Vainio <koti.mika.vainio@gmail.com>
2020-10-23 15:37:54 +0300

    Create DokumentoiPalveluTilit.ps1

---
+++ DokumentoiPalveluTilit.ps1
@@ -0,0 +1,3 @@
+# Dokumentoidaan palveluille luodut tilit (Managed Service Accounts)
+$Palvelutilit = Get-ADUser -Filter * -SearchBase "CN=Managed Service Accounts, DC=firma,DC=intra"
+$Palvelutilit | Out-GridView
